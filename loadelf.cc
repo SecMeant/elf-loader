b0a305273e43a6a01f2335a2bf2194ab3d51eb0b
SecMeant <secmeant@gmail.com>
2023-08-23 23:40:04 +0200

    loadelf.cc: Simple argument parsing

--- loadelf.cc
+++ loadelf.cc
@@ -1,4 +1,47 @@
-int main()
+#include <cstdio>
+#include <cstdlib>
+
+#include <getopt.h>
+
+static const char * elffile;
+
+int main(int argc, char **argv)
 {
+    static struct option long_options[] = {
+        { "file", required_argument, 0, 'f' },
+        { 0, 0, 0, 0 },
+    };
+
+    int option_index = 0;
+
+    while (1) {
+        int c = getopt_long(argc, argv, "f:", long_options, &option_index);
+
+        if (c == -1)
+            break;
+
+        switch (c) {
+            case 0:
+                /* Should not happen - we don't have any */
+                abort();
+
+            case 'f':
+                elffile = optarg;
+                break;
+
+            case '?':
+                /* getopt_long already printed an error message. */
+                break;
+
+            default:
+                abort();
+        }
+    }
+
+    if (!elffile) {
+        fprintf(stderr, "Elf file required\n");
+        return 1;
+    }
+
     return 0;
 }
